7fc5edb0378f0d4af215cc71e5d5ae67907a4481
Rafael Santos <rafaelnsantos1@gmail.com>
2017-09-05 13:52:39 -0300

    filtrar contas por ano e mes

--- graphql/mutations/conta/multiple.js
+++ graphql/mutations/conta/multiple.js
@@ -1,5 +1,7 @@
 import {
-    GraphQLList
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
   } from 'graphql'
   
   import contaType from '../../types/conta.js'
@@ -8,7 +10,14 @@ import {
 
   export default {
     type: new GraphQLList(contaType),
-    args: {},
+    args: {
+      ano: {
+        type: new GraphQLNonNull(GraphQLInt)
+      },
+      mes: {
+        type: new GraphQLNonNull(GraphQLInt)
+      }
+    },
     async resolve (root, params, options) {
       if(!options.user) 
         throw new Error('Usuario não logado')
@@ -17,7 +26,15 @@ import {
 
       if(!republica)
         throw new Error('Republica não encontrada')
-
-      return await ContaModel.find({republica: republica}).populate('pagou').sort('data')
+      return await ContaModel.find({
+        $and: [{
+          republica: republica}, {
+            data: {
+              "$gte": new Date(params.ano, params.mes-1, 1), 
+              "$lt": new Date(params.ano, params.mes, 1)
+            }
+          }
+        ]
+      }).populate('pagou').sort('data')
     }
   }
