d62922d3190aab2fc862fea07185247cbdfe9ecc
Rafael Santos <rafaelnsantos1@gmail.com>
2017-09-06 19:06:23 -0300

    hash user password async

--- models/user.js
+++ models/user.js
@@ -16,21 +16,13 @@ var UserSchema = new mongoose.Schema({
 })
 
 // Hash the user's password before inserting a new user TODO: procurar forma melhor
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', async function(next) {
   var user = this;
   if (this.isModified('password') || this.isNew) {
-    bcrypt.genSalt(10, function(err, salt) {
-      if (err) {
-        return next(err);
-      }
-      bcrypt.hash(user.password, salt, function(err, hash) {
-        if (err) {
-          return next(err);
-        }
-        user.password = hash;
-        next();
-      });
-    });
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(user.password, salt)
+    user.password = hash
+    next()
   } else {
     return next();
   }
