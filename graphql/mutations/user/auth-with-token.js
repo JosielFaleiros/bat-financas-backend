2ffa99b2e33b7674af2c025be52a4fc7a9840c50
Rafael Santos <rafaelnsantos1@gmail.com>
2017-09-14 18:24:31 -0300

    permissoes

--- graphql/mutations/user/auth-with-token.js
+++ graphql/mutations/user/auth-with-token.js
@@ -1,15 +1,18 @@
 import {
     GraphQLNonNull,
-    GraphQLString
+    GraphQLString,
+    GraphQLBoolean
   } from 'graphql'
   
 import userType from '../../types/novo-usuario-input.js'
 import UserModel from '../../../models/user.js'
 import jwt from 'jsonwebtoken'
 import RepublicaModel from '../../../models/republica.js'
+import MoradorModel from '../../../models/morador.js'
+import { AbilityBuilder, Ability } from 'casl'
 
 export default {
-  type: GraphQLString,
+  type: GraphQLBoolean,
   args: {
     token: {
       name: 'token',
@@ -17,9 +20,31 @@ export default {
     }
   },
   async resolve (root, params, options) {
-    let teste = await jwt.verify(params.token, 'superSecret')
+    try {
+      var teste = await jwt.verify(params.token, 'superSecret')
+    } catch (err){
+      return false
+    }
     options.user = await UserModel.findById(teste._doc._id)
     options.republica = await RepublicaModel.findOne({user: options.user})
+    if(!options.republica) {
+      let morador = await MoradorModel.findOne({user: options.user}).populate('republica')
+      if(morador)
+        options.republica = morador.republica
+    }
+    if(options.republica)
+      defineAbilities(options)
+    return true
   }
 }
   
+function defineAbilities(options) {
+  const { rules, can } = AbilityBuilder.extract()
+
+  if (options.user._id.equals(options.republica.user)) {
+    can('manage', ['Conta', 'Morador'])
+    can('update', ['Republica'])
+  }
+
+  options.abilities = new Ability(rules)
+}
